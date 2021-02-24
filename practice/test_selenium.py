c1131642035b8de4cf9883c57ab984fc8ed66689
xylona <593872142@qq.com>
2021-02-24 17:23:47 +0800

    web控件定位练习

--- practice/test_selenium.py
+++ practice/test_selenium.py
@@ -2,6 +2,8 @@ import selenium
 from selenium import webdriver
 from time import sleep
 
+from selenium.webdriver.common.by import By
+
 
 class TestSelenium():
     def setup(self):
@@ -15,6 +17,10 @@ class TestSelenium():
 
     def test_selenium(self):
         self.driver.get("https://www.baidu.com/")
+        # 点击新闻后会形成页面跳转，后续的操作在新页面上完成，现在的知识还不足以写出来，先搁置
         self.driver.find_element_by_link_text("新闻").click()
-        self.driver.find_element_by_css_selector(".home-banner-cell").click()
-        # self.driver.find_element_by_xpath("//div[@id='pane-news']/div/ul/li[2]/strong/a/b").click()
+        # 通过XPATH定位法定位到的元素，因为两个a元素是并行的，并且a下面都有b,所以用//a/b[1]定位不到想要的元素
+        self.driver.find_element(By.XPATH, '// *[ @ id = "pane-news"] // a[1] / b').click()
+        # 通过console拷贝出来的全路径，当你自己通过语句查不到想到的元素时，可以用这个方法，且定位的最精准，但就不是那么高阶了
+        # self.driver.find_element(By.XPATH, '//*[@id="pane-news"]/div/ul/li[2]/strong/a[1]/b').click()
+
--- practice/test_selenium_显式等待.py
+++ practice/test_selenium_显式等待.py
@@ -11,13 +11,17 @@ class TestWait:
         # implicitly_wait 隐式等待三秒，适用于全局
         self.driver.implicitly_wait(3)
 
+    def teardwon(self):
+        self.driver.quit()
+
     def test_wait(self):
-        self.driver.find_element(By.XPATH, '//*[@title="原创精华文章,有100元奖金"]').click()
+        # 元素中ID和NAME虽然是唯一的，但是不排除每一次刷新页面后，或者点击某些元素后，发生变更的情况，所以要根据情况选择每次操作都不变的元素来定位
+        self.driver.find_element(By.XPATH, '//*[@id="ember41"]').click()
 
         # WebDriverWait 显式等待,先定义一个方法wait,并且方法会返回一个判断，WebDriverWait定义等待10秒，直到wait方法通过时，才执行下一步操作，不然就报错
         def wait(x):
             # 返回判断，寻找大于等于一的路径数据（至少一个搜索项）
-            return len(self.driver.find_elements(By.XPATH, '//*[@class="posts sortable num"]')) >= 1
+            return len(self.driver.find_elements(By.XPATH, '//*[@class="table-heading"]')) >= 1
 
         WebDriverWait(self.driver, 10).until(wait)
         self.driver.find_element(By.XPATH, '//*[@title="在最近的一年，一月，一周或一天最活跃的主题"]').click()
