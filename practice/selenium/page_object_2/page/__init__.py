e2b6c528b7f84b3c87993c5cd87b0e09bd7ccf58
Administrator <593872142@qq.com>
2021-03-02 23:03:01 +0800

    pageobject练习

---
+++ practice/selenium/page_object_2/page/__init__.py
@@ -0,0 +1,4 @@
+"""
+__author__ = 'xylona'
+__time__ = ''
+"""
---
+++ practice/selenium/page_object_2/testcase/test_contact.py
@@ -0,0 +1,19 @@
+from web.seleniumPO.page.main_page import MainPage
+
+
+class TestContact:
+    def setup(self):
+        self.mainpage = MainPage()
+
+    def test_addmember(self):
+        username = "aaaa_0005"
+        account = "aaaa_0005"
+        phonenum = "13012345605"
+        page = self.mainpage.goto_add_member()
+        page.add_member(username, account, phonenum)
+        names = page.get_member()
+        print(names)
+        assert username in names
+
+    def teardown(self):
+        self.mainpage.quit_driver()
