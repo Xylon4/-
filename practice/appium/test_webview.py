0cc59e090fc6430f27e4cc82e5d1902452ae9006
Xylon4 <593872142@qq.com>
2021-03-19 17:14:05 +0800

    添加窗口切换

--- practice/appium/test_webview.py
+++ practice/appium/test_webview.py
@@ -19,7 +19,7 @@ class TestWebview:
             'deviceName': 'emulator-5554',
             'chromedriverExecutable': 'C:\Program Files\Appium/\/resources/\/app/\/node_modules/\/appium/\/node_modules/\/appium-chromedriver\chromedriver\win\chromedriver_52.0.2743'
         }
-        self.driver = webdriver.Remote("http://localhost/wd/hub", des_caps)
+        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", des_caps)
         self.driver.implicitly_wait(10)
 
     def teardown(self):
@@ -29,9 +29,14 @@ class TestWebview:
     def test_webview(self):
         self.driver.find_element_by_xpath("//*[@text='交易']").click()
         self.driver.find_element(MobileBy.XPATH, "//*[@content-desc='A股开户']").click()
+        # 进入webview页面，需要切换窗口
+        switch_window = self.driver.window_handles[-1]
+        self.driver.switch_to.window(switch_window)
+
         # 设置显式等待，等待页面加载完再操作
         phone_num = (By.XPATH, "//*[@resource-id='phone-number']")
-        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(phone_num))
+        WebDriverWait(self.driver, 20).until(expected_conditions.visibility_of_element_located(phone_num))
+        self.driver.find_element_by_xpath("//*[@resource-id='phone-number']").click()
         self.driver.find_element_by_xpath("//*[@resource-id='phone-number']").send_keys("13012345678")
         self.driver.find_element_by_xpath("//*[@content-desc='获取验证码']").click()
         self.driver.find_element_by_xpath("//*[@resource-id='code']").send_keys("123456")
