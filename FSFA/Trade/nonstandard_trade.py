22833fe9a6ba72797bd601d0c0e852773d6e0821
xylona <593872142@qq.com>
2021-06-23 17:30:26 +0800

    FSFA-非标交易首期

---
+++ FSFA/Trade/nonstandard_trade.py
@@ -0,0 +1,54 @@
+# 理财估值核算系统交易端自动化测试用例
+# 利率型项目交易-买入及成交确认
+from time import sleep
+
+from selenium.webdriver.common.keys import Keys
+
+from FSFA.basepage_FSFA import BasePageFsfa
+
+
+class NonstandardTrade(BasePageFsfa):
+    # 利率型项目交易买入及成交确认
+    def nonstandardtrade1(self):
+        UNIT_CODE = 'FB0618'
+        I_CODE = 'LBS20210601'
+        Counterparties = "杭州银行股份有限公司"
+        # 点击交易管理
+        self.findxpath_click('//*[@id="navId"]/li[5]/a')
+        # 点击利率型项目交易
+        self.findxpath_click('//*[@id="floatMenu"]/dl[5]/dd[1]/a')
+        # 点击新增
+        self.findxpath_click('//div[3]/div[2]/div[2]//div[1]/span/div/div[1]//a[2]/span/span/span[1]')
+        # 选择账套
+        self.findxpath_sendkey('//*[@name="package_code"]', UNIT_CODE)
+        sleep(1)
+        self.findxpath_click('//div[30]/div[3]/div/table/tbody/tr[2]/td/div/span')
+        # 选择交易对手
+        self.findxpath_sendkey('//*[@name="partyId"]', Counterparties)
+        sleep(1)
+        party_id = self.findxpath('//*[@name="partyId"]')
+        party_id.send_keys(Keys.SPACE)
+        sleep(2)
+        party_id.send_keys(Keys.ARROW_DOWN)
+        party_id.send_keys(Keys.ENTER)
+        # 选择资产
+        self.findxpath_sendkey('//*[@name="i_code"]', I_CODE)
+        sleep(1)
+        asset = self.findxpath('//*[@name="i_code"]')
+        asset.send_keys(Keys.ARROW_DOWN)
+        asset.send_keys(Keys.ENTER)
+        # 点击保存
+        self.findxpath_click('//div[3]/div[2]/div[2]/div/div[2]/div/div[1]/div[3]/div/div/a[2]/span/span/span[1]')
+        sleep(5)
+        # 点击确定
+        self.findxpath_click('//*[@id="button-1005-btnIconEl"]')
+        # 点击复核
+        self.findxpath_click('/html/body/div[3]/div[2]/div[2]/div/div[2]/div/div[1]/div[3]/div/div/a[5]')
+        sleep(3)
+        # 点击确定
+        self.findxpath_click('//*[@id="button-1005-btnIconEl"]')
+        return True
+
+    # 结算非标首期交易
+    def nonstandardtrade2(self):
+        pass
