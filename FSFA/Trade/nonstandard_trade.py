dc638d15989013cebb088e6e65bf69c5e256d4e7
xylona <593872142@qq.com>
2021-06-29 16:02:26 +0800

    FSFA-非标交易结算及银行间费用摊销处理完善

--- FSFA/Trade/nonstandard_trade.py
+++ FSFA/Trade/nonstandard_trade.py
@@ -51,4 +51,55 @@ class NonstandardTrade(BasePageFsfa):
 
     # 结算非标首期交易
     def nonstandardtrade2(self):
-        pass
+        I_CODE = 'LBS20210601'
+        # 点击清算管理
+        self.findxpath_click('//*[@id="navId"]/li[6]/a')
+        # 点击待复核交易指令
+        self.findxpath_click('//*[@id="floatMenu"]/dl[1]/dd[1]/a')
+        # 定位交易
+        self.findxpath_click('//div[3]/div[2]/div[2]/div/div/div[1]/a/span/span/span[2]')
+        self.findxpath_click('//div[7]/div/div[2]/div[2]/div[2]/a')
+        self.findxpath_click('//div[2]/span/div/div[2]/span/div/table[4]/tbody/tr/td[2]/table/tbody/tr/td[1]/input')
+        sleep(1)
+        self.findxpath_sendkey('//div[2]/span/div/div[2]/span/div/table[4]/tbody/tr/td[2]/table/tbody/tr/td[1]/input',
+                               I_CODE)
+        sleep(1)
+        bond_code = self.findxpath(
+            '//div[2]/span/div/div[2]/span/div/table[4]/tbody/tr/td[2]/table/tbody/tr/td[1]/input')
+        bond_code.send_keys(Keys.ARROW_DOWN)
+        bond_code.send_keys(Keys.ENTER)
+        sleep(1)
+        self.findxpath_click('//div[3]/div/div/a[1]')
+        sleep(1)
+        self.findxpath_click('//div[3]/div/div/a[2]')
+        sleep(3)
+        self.findxpath_click('//div/div/div[3]/div/table/tbody/tr/td[2]/div')
+        # 点击复核
+        self.findxpath_click('/html/body/div[3]/div[2]//div/div/div[1]/div/div/a[2]')
+        # 点击弹窗中的是
+        self.findxpath_click('//*[@id="button-1006-btnIconEl"]')
+        # 点击确定
+        self.findxpath_click('//*[@id="button-1005-btnIconEl"]')
+        return True
+
+    # 银行间费用摊销复核
+    def nonstandardtrade3(self):
+        I_CODE = "FB0618"
+        # 点击交易管理
+        self.findxpath_click('//*[@id="navId"]/li[5]/a')
+        # 点击银行间费用摊销
+        self.findxpath_click('//*[@id="floatMenu"]/dl[9]/dd[6]/a')
+        # 定位账套
+        self.findxpath_sendkey('//*[@name="wmsunitname$array"]', I_CODE)
+        sleep(1)
+        self.findxpath_click('//div[3]/div/table/tbody/tr[2]/td/div/span')
+        self.findxpath_click('//div[3]/div[2]//div/div/div[1]/span/div/div[2]/div/div/a[1]')
+        sleep(1)
+        self.findxpath_click('//div[3]/div[2]//div/div/div[3]/div/table/tbody/tr/td[2]/div')
+        # 点击复核
+        self.findxpath_click('//div[3]/div[2]//div/div/div[1]/span/div/div[1]/div/div/a[6]')
+        # 点击弹窗中的是
+        self.findxpath_click('//*[@id="button-1006-btnIconEl"]')
+        # 点击确定
+        self.findxpath_click('//*[@id="button-1005-btnIconEl"]')
+        return True
--- FSFA/Trade/test_trade.py
+++ FSFA/Trade/test_trade.py
@@ -1,18 +1,40 @@
 from FSFA.Trade.bond_trade import BondTrade
+from FSFA.Trade.nonstandard_trade import NonstandardTrade
 
 
 class TestTrade:
     def setup(self):
-        self.bond_trade = BondTrade()
+        pass
 
     def teardown(self):
-        self.bond_trade.end()
+        pass
 
     def test_bond_trade1(self, stagemark):
+        self.bond_trade = BondTrade()
         assert self.bond_trade.bondtrade1()
+        self.bond_trade.end()
 
     def test_bond_trade2(self, stagemark):
+        self.bond_trade = BondTrade()
         assert self.bond_trade.bondtrade2()
+        self.bond_trade.end()
 
     def test_bond_trade3(self, stagemark):
+        self.bond_trade = BondTrade()
         assert self.bond_trade.bondtrade3()
+        self.bond_trade.end()
+
+    def test_nonstandard_trade1(self, stagemark):
+        self.nonstandard_trade = NonstandardTrade()
+        assert self.nonstandard_trade.nonstandardtrade1()
+        self.nonstandard_trade.end()
+
+    def test_nonstandard_trade2(self, stagemark):
+        self.nonstandard_trade = NonstandardTrade()
+        assert self.nonstandard_trade.nonstandardtrade2()
+        self.nonstandard_trade.end()
+
+    def test_nonstandard_trade3(self, stagemark):
+        self.nonstandard_trade = NonstandardTrade()
+        assert self.nonstandard_trade.nonstandardtrade3()
+        self.nonstandard_trade.end()
