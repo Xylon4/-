677b26630bd5fc5dc3eccaceb19f7644f8b17b04
xylona <593872142@qq.com>
2021-07-21 17:39:58 +0800

    FSFA-环境初始化部署文档及数据驱动优化

--- FSFA/Login/test_login.py
+++ FSFA/Login/test_login.py
@@ -5,9 +5,9 @@ from selenium import webdriver
 
 
 class TestLogin:
-    def setup(self):
+    def setup(self, get_ip):
         self.driver = webdriver.Chrome()
-        self.driver.get("http://192.168.64.131:8080/xIR_J2EE")
+        self.driver.get(get_ip[0])
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
 
@@ -19,16 +19,16 @@ class TestLogin:
         message1 = self.driver.find_element_by_xpath('//*[@id="ASPxLabel_Message"]').text
         assert message1 == "请输入用户名"
 
-    def test_login2(self):
-        self.driver.find_element_by_xpath("//*[@id='txtAccount_I']").send_keys("7654321")
-        self.driver.find_element_by_xpath("//*[@id='txtPwd_I']").send_keys("1234567")
+    def test_login2(self, get_fail_token):
+        self.driver.find_element_by_xpath("//*[@id='txtAccount_I']").send_keys(get_fail_token[0])
+        self.driver.find_element_by_xpath("//*[@id='txtPwd_I']").send_keys(get_fail_token[1])
         self.driver.find_element_by_xpath("//*[@id='subBtn']").click()
         message2 = self.driver.find_element_by_xpath('//*[@id="ASPxLabel_Message"]').get_attribute('textContent')
         assert message2 == "用户名或密码错误"
 
-    def test_login3(self):
-        self.driver.find_element_by_xpath("//*[@id='txtAccount_I']").send_keys("T999999")
-        self.driver.find_element_by_xpath("//*[@id='txtPwd_I']").send_keys("123456")
+    def test_login3(self, get_success_token):
+        self.driver.find_element_by_xpath("//*[@id='txtAccount_I']").send_keys(get_success_token[0])
+        self.driver.find_element_by_xpath("//*[@id='txtPwd_I']").send_keys(get_success_token[1])
         self.driver.find_element_by_xpath("//*[@id='subBtn']").click()
         bussiness_date = self.driver.find_element_by_xpath('//*[@id="switchBox"]/h2').text
         assert bussiness_date == "菜单"
--- FSFA/conftest.py
+++ FSFA/conftest.py
@@ -1,12 +1,45 @@
 import pytest
+import yaml
 
 
-# 创建装饰器方法stagemark，用来标记用例的开始与结束
+# 定义装饰器方法stagemark，用来标记用例的开始与结束
 @pytest.fixture(scope="module")
 def stagemark():
     print("自动化案例执行开始")
     yield
     print("自动化案例执行结束")
+
+
+# 读取外部文件并定义,路径中的.代表根目录"FSFA"
+with open("./login.yaml") as f:
+    datas = yaml.safe_load(f)['login_data']
+    ip = datas['ipaddr']
+    fail_token = datas['fail_token']
+    success_token = datas['success_token']
+
+
+# 定义IP的装饰器方法
+@pytest.fixture(params=ip)
+def get_ip(request):
+    ip = request.param
+    print(f"测试数据为：{ip}")
+    yield ip
+
+
+# 定义错误用户密码的装饰器方法
+@pytest.fixture(params=fail_token)
+def get_fail_token(request):
+    fail_token = request.param
+    print(f"测试数据为：{fail_token}")
+    yield fail_token
+
+
+# 定义正确用户密码的装饰器方法
+@pytest.fixture(params=success_token)
+def get_success_token(request):
+    success_token = request.param
+    print(f"测试数据为：{success_token}")
+    yield success_token
 
 
 # 解决命令行执行找不到调用的包
