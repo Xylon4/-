84d1b0affd79b7cd140ae690a17a346124e36d50
Administrator <593872142@qq.com>
2021-11-02 17:27:47 +0800

    XAMS-万能自动化测试工具底层方法封装完成及估值表页面完成

--- XAMS/Report/Finance/valuation.py
+++ XAMS/Report/Finance/valuation.py
@@ -1,9 +1,53 @@
 # 估值表自动化测试用例
 # 功能描述：投组每日估值对账数据
+from time import sleep
+
+from selenium.webdriver.common.keys import Keys
+
+from XAMS.Tool.test_excel import TestExcel
 from XAMS.basepage_XAMS import BasePageFsfa
 
 
 class Valuation(BasePageFsfa):
     # 自动化测试工具案例
-    def valuation_excel(self):
-        pass
+    def valuation_excel(self, menu, value):
+        print(menu)
+        print(value)
+        self.base = TestExcel()
+        # 点击一级菜单
+        self.findxpath_click(self.base.first_menu().get(menu[0]))
+        # 点击二级菜单
+        self.findxpath_click(self.base.second_menu().get(f'{menu[0]}-{menu[1]}'))
+        # 根据自定义顺序执行操作
+        l = len(menu)
+        n = 2
+        while n < l:
+            if menu[n] == '导出':
+                self.findxpath_click(self.base.valuation_xpath().get(menu[n]))
+            elif menu[n] == 'Excel(当前页)':
+                self.findxpath_click(self.base.valuation_xpath().get(menu[n]))
+            elif menu[n] == 'Excel(所有数据)':
+                self.findxpath_click(self.base.valuation_xpath().get(menu[n]))
+            elif menu[n] == '批量导出':
+                self.findxpath_click(self.base.valuation_xpath().get(menu[n]))
+            elif menu[n] == '生成估值表':
+                self.findxpath_click(self.base.valuation_xpath().get(menu[n]))
+            elif menu[n] == '查询日期':
+                if value[n] == '置空':
+                    date = self.findxpath(self.base.valuation_xpath().get(menu[n]))
+                    date.send_keys(Keys.CONTROL, 'a')
+                    date.send_keys(Keys.BACK_SPACE)
+                else:
+                    date = self.findxpath(self.base.valuation_xpath().get(menu[n]))
+                    date.send_keys(Keys.CONTROL, 'a')
+                    date.send_keys(Keys.BACK_SPACE)
+                    date.send_keys(value[n])
+            elif menu[n] == '投组单元':
+                self.findxpath_sendkey(self.base.valuation_xpath().get('投组单元'), value[n])
+                sleep(1)
+                self.findxpath_click(self.base.valuation_xpath().get('投组下拉选择'))
+            elif menu[n] == '搜索':
+                self.findxpath_click(self.base.valuation_xpath().get(menu[n]))
+                sleep(1)
+            n = n + 1
+        return True
--- XAMS/Report/test_report.py
+++ XAMS/Report/test_report.py
@@ -12,7 +12,18 @@ from XAMS.conftest import stagemark
 class TestReport:
     # 万能导入用例
     def test_universal(self):
-        pass
+        self.list = TestExcel()
+        count = len(self.list.code_list())
+        n = 0
+        while n < count:
+            code = self.list.code_list()[n]
+            menu = self.list.group_ele_dic().get(code)
+            value = self.list.group_value_dic().get(code)
+            second_menu = f'{menu[0]}-{menu[1]}'
+            if second_menu == '估值管理-估值表':
+                self.test_valuation_excel(stagemark, menu, value)
+            n = n + 1
+        print(f'自动化案例执行完毕，共{count}条')
 
     @pytest.mark.skip
     # 根据excel导入内容选择执行案例
@@ -63,7 +74,7 @@ class TestReport:
         print(f"{sheet2}自动化操作执行完毕")
 
     @pytest.mark.skip
-    def test_valuation_excel(self, stagemark):
+    def test_valuation_excel(self, stagemark, menu, value):
         self.valuation = Valuation()
-        assert self.valuation.valuation_excel()
+        assert self.valuation.valuation_excel(menu, value)
         print(f"{sheet4}自动化操作执行完毕")
