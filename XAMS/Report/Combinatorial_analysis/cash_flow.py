17092e5d418cb652cf8228e803f55ed5c9cb6dc3
Administrator <593872142@qq.com>
2021-11-24 11:11:37 +0800

    XAMS-现金流明细表(新)完成

--- XAMS/Report/Combinatorial_analysis/cash_flow.py
+++ XAMS/Report/Combinatorial_analysis/cash_flow.py
@@ -1,18 +1,18 @@
-# 现金流缺口表(新)自动化测试用例
-# 功能描述：统计投组头寸信息
+# 现金流明细表(新)自动化测试用例
+# 功能描述：统计投组现金流信息
 from time import sleep
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-from XAMS.Report.conftest import sheet24
+from XAMS.Report.conftest import sheet25
 from XAMS.Tool.test_excel import TestExcel
 from XAMS.basepage_XAMS import BasePageXams
 
 
-class CashGap(BasePageXams):
+class CashFlow(BasePageXams):
     # 模拟操作自动化案例
-    def cash_gap_excel(self, menu, value):
+    def cash_flow_excel(self, menu, value):
         print(menu)
         print(value)
         self.base = TestExcel()
@@ -25,43 +25,59 @@ class CashGap(BasePageXams):
         n = 2
         while n < l:
             if menu[n] == '导出':
-                self.findxpath_click(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                self.findxpath_click(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
             elif menu[n] == '投组单元':
                 if value[n] == '置空':
-                    unit = self.findxpath(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                    unit = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
                     unit.send_keys(Keys.CONTROL, 'a')
                     unit.send_keys(Keys.BACK_SPACE)
                 else:
-                    unit = self.findxpath(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                    unit = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
                     unit.send_keys(Keys.CONTROL, 'a')
                     unit.send_keys(Keys.BACK_SPACE)
                     unit.send_keys(value[n])
                     sleep(1)
-                    self.findxpath_click(self.base.sheet_xpath_dic(sheet24).get('投组下拉选择'))
+                    self.findxpath_click(self.base.sheet_xpath_dic(sheet25).get('投组下拉选择'))
+            elif menu[n] == '现金流类型':
+                if value[n] not in ['产品资金流入', '产品资金流出', '资产资金流入', '资产资金流出', '置空']:
+                    print(f'值"{value[n]}"输入错误，请检查')
+                    return False
+                elif value[n] == '置空':
+                    cashtype = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
+                    cashtype.send_keys(Keys.CONTROL, 'a')
+                    cashtype.send_keys(Keys.BACK_SPACE)
+                else:
+                    cashtype = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
+                    cashtype.send_keys(Keys.CONTROL, 'a')
+                    cashtype.send_keys(Keys.BACK_SPACE)
+                    cashtype.send_keys(value[n])
+                    sleep(1)
+                    cashtype.send_keys(Keys.ARROW_DOWN)
+                    cashtype.send_keys(Keys.ENTER)
             elif menu[n] == '开始日期':
                 if value[n] == '置空':
-                    startdate = self.findxpath(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                    startdate = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
                     startdate.send_keys(Keys.CONTROL, 'a')
                     startdate.send_keys(Keys.BACK_SPACE)
                 else:
-                    startdate = self.findxpath(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                    startdate = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
                     startdate.send_keys(Keys.CONTROL, 'a')
                     startdate.send_keys(Keys.BACK_SPACE)
                     startdate.send_keys(value[n])
             elif menu[n] == '结束日期':
                 if value[n] == '置空':
-                    enddate = self.findxpath(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                    enddate = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
                     enddate.send_keys(Keys.CONTROL, 'a')
                     enddate.send_keys(Keys.BACK_SPACE)
                 else:
-                    enddate = self.findxpath(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                    enddate = self.findxpath(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
                     enddate.send_keys(Keys.CONTROL, 'a')
                     enddate.send_keys(Keys.BACK_SPACE)
                     enddate.send_keys(value[n])
             elif menu[n] == '搜索':
-                self.findxpath_click(self.base.sheet_xpath_dic(sheet24).get(menu[n]))
+                self.findxpath_click(self.base.sheet_xpath_dic(sheet25).get(menu[n]))
                 # sleep(1)  # 强制等待用来过渡到显式等待
-                # wait = (By.XPATH, self.base.sheet_xpath_dic(sheet24).get('加载等待'))
+                # wait = (By.XPATH, self.base.sheet_xpath_dic(sheet25).get('加载等待'))
                 # self.wait_for_miss(120, wait)
             else:
                 print(f'操作元素"{menu[n]}"输入错误，请检查')
--- XAMS/Report/test_report.py
+++ XAMS/Report/test_report.py
@@ -6,6 +6,7 @@ from XAMS.Report.CBRC.deal_registration import DealRegistration
 from XAMS.Report.CBRC.product_duration_registration import ProductDurationRegistration
 from XAMS.Report.Combinatorial_analysis.asset_detail import AssetDetail
 from XAMS.Report.Combinatorial_analysis.asset_detail_penetration import AssetDetailPenetration
+from XAMS.Report.Combinatorial_analysis.cash_flow import CashFlow
 from XAMS.Report.Combinatorial_analysis.cash_gap import CashGap
 from XAMS.Report.Finance.valuation import Valuation
 from XAMS.Report.Financial.asset_liability import AssetLiability
@@ -27,7 +28,7 @@ from XAMS.Report.PBC.product_term import ProductTerm
 from XAMS.Report.PBC.product_unpaid import ProductUnpaid
 from XAMS.Report.conftest import Excel_basedata, sheet1, sheet2, sheet3, sheet4, sheet5, sheet6, sheet7, sheet8, sheet9, \
     sheet10, sheet11, sheet12, sheet13, sheet14, sheet15, sheet16, sheet17, sheet18, sheet19, sheet20, sheet21, sheet22, \
-    sheet23, sheet24
+    sheet23, sheet24, sheet25
 from XAMS.Tool.test_excel import TestExcel
 
 
@@ -91,6 +92,8 @@ class TestReport:
                     self.test_asset_detail_excel(stagemark, menu, value, address)
                 elif second_menu == '投组管理-现金流缺口表(新)':
                     self.test_cash_gap_excel(stagemark, menu, value, address)
+                elif second_menu == '投组管理-现金流明细表(新)':
+                    self.test_cash_flow_excel(stagemark, menu, value, address)
                 else:
                     print("模拟操作案例：该报表暂不支持，请修改用例")
             elif test_goal == '升级对比':
@@ -445,4 +448,11 @@ class TestReport:
         self.cash_gap = CashGap(address)
         assert self.cash_gap.cash_gap_excel(menu, value)
         print(f"{sheet24}模拟操作执行完毕")
+        print('-----------------------这是案例分割线-----------------------')
+
+    @pytest.mark.skip
+    def test_cash_flow_excel(self, stagemark, menu, value, address):
+        self.cash_flow = CashFlow(address)
+        assert self.cash_flow.cash_flow_excel(menu, value)
+        print(f"{sheet25}模拟操作执行完毕")
         print('-----------------------这是案例分割线-----------------------')
