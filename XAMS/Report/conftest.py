ce4acff6f54ddad0986cc7c98e8083ebdf4d3a29
xylona <593872142@qq.com>
2021-12-02 17:54:59 +0800

    XAMS-代码结构优化/拓展资产交易模块

--- XAMS/Report/conftest.py
+++ XAMS/Report/conftest.py
@@ -3,18 +3,18 @@ import yaml
 
 # 读取外部文件并定义,路径中的.代表实际调用方法的文件的上一级目录,./.代表实际调用方法的文件的上两级目录
 # report文件读取 指定文件内容读取格式为gb18030，并且需要设置文件编码格式为相同编码
-with open("././report.yaml", encoding='gb18030', errors='ignore') as f:
-    datas = yaml.safe_load(f)['report']
-    registry = datas['registry']
-    nonstandard = datas['nonstandard']
+# with open("././report.yaml", encoding='gb18030', errors='ignore') as f:
+#     datas = yaml.safe_load(f)['report']
+#     registry = datas['registry']
+#     nonstandard = datas['nonstandard']
 
 
 # 定义资产池注册表的装饰器方法
-@pytest.fixture(params=registry)
-def get_registry(request):
-    registry = request.param
-    print(f"测试数据为：{registry}")
-    return registry
+# @pytest.fixture(params=registry)
+# def get_registry(request):
+#     registry = request.param
+#     print(f"测试数据为：{registry}")
+#     return registry
 
 
 Excel_basedata = 'E:\菜单基础数据维护.xlsx'
